579cef0f6dc4e2a4bb65726f34ea739b58e85101
Yash <yash.sharma@tothenew.com>
2023-11-30 05:33:51 +0530

    first commit at 30 Nov

---
+++ src/main/java/com/schoolbackend/SchoolbackendApplication.java
@@ -0,0 +1,13 @@
+package com.schoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolbackend/SchoolbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
