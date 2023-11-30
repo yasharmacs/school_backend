a5dc36c1742f9821c50941025b0706379f7d6f1a
Yash <yash.sharma@tothenew.com>
2023-11-30 06:43:00 +0530

    Adding Teacher structure

--- src/main/java/com/schoolbackend/resoruce/AccountResource.java
+++ src/main/java/com/schoolbackend/resoruce/AccountResource.java
@@ -4,6 +4,7 @@ import com.schoolbackend.model.StudentInfoVM;
 import com.schoolbackend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("api/v1")
@@ -11,6 +12,7 @@ public class AccountResource {
 
     @Autowired
     private StudentService studentService;
+    @PostMapping(value="/addStudent")
     public ResponseEntity<StudentInfoVM> addStudent(StudentInfoVM studentInfoVM){
         return studentService.addStudent(studentInfoVM);
     }
